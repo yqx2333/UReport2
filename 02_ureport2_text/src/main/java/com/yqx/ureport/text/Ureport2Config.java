c928168e4ddb3b4dad8c67d10d40418397994259
YuQingXi <840174265@qq.com>
2021-01-26 10:51:45 +0800

    添加了UReport2配置文件类,添加方法; 修改了application.yaml文件 添加端口和数据库配置

--- 02_ureport2_text/src/main/java/com/yqx/ureport/text/Ureport2Config.java
+++ 02_ureport2_text/src/main/java/com/yqx/ureport/text/Ureport2Config.java
@@ -1,20 +1,30 @@
 package com.yqx.ureport.text;
 
+import com.bstek.ureport.console.UReportServlet;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+
+import javax.servlet.Servlet;
 
 /**
  * UReport2配置类
  */
 
+// @ImportResource 用于将xml文件加载到Spring配置文件中
 @ImportResource( "classpath:ureport-console-context.xml" )
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan( basePackages = "com.yqx.ureport.text")
 public class Ureport2Config {
 
-
+    // 注入Bean,将UReport2加载到Servlet对象中
+    @Bean
+    public ServletRegistrationBean<Servlet> buildUreportServlet(){
+        return new ServletRegistrationBean<Servlet>(new UReportServlet(), "/ureport/*");
+    }
 
 }
