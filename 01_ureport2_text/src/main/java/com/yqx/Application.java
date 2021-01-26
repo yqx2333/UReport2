65ec626f60d7513d190a3c0d09d4b49ca383a586
YuQingXi <840174265@qq.com>
2021-01-26 10:31:48 +0800

    添加 context.xml文件和 config.properties文件

--- 01_ureport2_text/src/main/java/com/yqx/Application.java
+++ 01_ureport2_text/src/main/java/com/yqx/Application.java
@@ -5,8 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication	// 启动类
+// 监听加载UReport2提供的Spring配置文件 ureport-console-context.xml文件
+@ImportResource( "classpath:context.xml" )
 public class Application {
 
 	public static void main(String[] args) {
@@ -19,7 +22,7 @@ public class Application {
 	@Bean
 	public ServletRegistrationBean buildReportServlet(){
 		// /ureport/*    访问该路径下所有的文件
-		return new ServletRegistrationBean( new UReportServlet(),"/ureport/*")
+		return new ServletRegistrationBean( new UReportServlet(),"/ureport/*");
 	}
 
 }
