5184099b93816d1616cf95661e228fc90bb57ddb
YuQingXi <840174265@qq.com>
2021-01-26 10:15:46 +0800

    添加了UReport2方法 该方法主要是将UReport2添加到Servlet中

--- 01_ureport2_text/src/main/java/com/yqx/Application.java
+++ 01_ureport2_text/src/main/java/com/yqx/Application.java
@@ -1,13 +1,25 @@
 package com.yqx;
 
+import com.bstek.ureport.console.UReportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication	// 启动类
 public class Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+
+	}
+
+	// 添加UReport2到Servlet
+	@Bean
+	public ServletRegistrationBean buildReportServlet(){
+		// /ureport/*    访问该路径下所有的文件
+		return new ServletRegistrationBean( new UReportServlet(),"/ureport/*")
 	}
 
 }
